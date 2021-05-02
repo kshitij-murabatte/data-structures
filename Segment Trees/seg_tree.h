14e63b1c5807c3e2672b5ab78bbd289908be8d17
Kshitij <kshitijmurabatte@gmail.com>
2021-05-03 04:43:11 +0530

    Point and Range updates
    Code refactoring, improved constructors initialization, now objects don't store
    copy of base array

--- Segment Trees/seg_tree.h
+++ Segment Trees/seg_tree.h
@@ -4,8 +4,12 @@
 // Segment tree
 class SegTree
 {
-    // Copy of user provided linear structure
+    // User provided linear structure
     int *base;
+    std::vector<int> *vec;
+
+    // Removes need to make another function
+    int base_type;
 
     // Array to store the segement tree
     int *tree;
@@ -19,8 +23,12 @@ class SegTree
     // Type of segement tree
     int type;
 
-    // For performance improvement
-    int q_start, q_end;
+    // For minor performance improvement
+    // these variables act as place holders
+    // for variables that do not change value
+    // during recursion, removing
+    // need of passing them in recursive calls
+    int q_start, q_end, u_start, u_end;
 
     // Builds the segement tree
     // start, end -> indices of user array copy (base)
@@ -30,8 +38,16 @@ class SegTree
         // Return at single point
         if (start == end)
         {
-            tree[index] = base[start];
-            return;
+            // For vector base / array base
+            switch (base_type)
+            {
+            case 0:
+                tree[index] = base[start];
+                return;
+            case 1:
+                tree[index] = (*vec)[start];
+                return;
+            }
         }
 
         int mid = start + ((end - start) / 2);
@@ -45,16 +61,16 @@ class SegTree
         {
         case 0:
             tree[index] = tree[2 * index + 1] + tree[2 * index + 2];
-            break;
+            return;
         case 1:
             tree[index] = std::max(tree[2 * index + 1], tree[2 * index + 2]);
-            break;
+            return;
         case 2:
             tree[index] = std::min(tree[2 * index + 1], tree[2 * index + 2]);
-            break;
+            return;
         case 3:
             tree[index] = (tree[2 * index + 1] ^ tree[2 * index + 2]);
-            break;
+            return;
         }
     }
 
@@ -91,7 +107,7 @@ class SegTree
             left = _query(b_start, mid, 2 * index + 1);
 
         // Get right sub tree answer
-        if (mid + 1 <= q_end)
+        if (mid  < q_end)
             right = _query(mid + 1, b_end, 2 * index + 2);
 
         // Returning answer based on type of segement tree
@@ -110,6 +126,91 @@ class SegTree
         return (left ^ right);
     }
 
+    // point_update utility function
+    void _update(int b_start, int b_end, int index)
+    {
+        // Found the update point
+        if (b_start == q_start and q_start == b_end)
+        {
+            tree[index] = q_end;
+            return;
+        }
+
+        // Partial overlap
+        int mid = b_start + ((b_end - b_start) / 2);
+
+        // Search in the left sub-tree
+        if (mid >= q_start)
+            _update(b_start, mid, 2 * index + 1);
+
+        // Search in the left right-tree
+        if (mid < q_start)
+            _update(mid + 1, b_end, 2 * index + 2);
+
+        // To switch different types of segement trees
+        switch (type)
+        {
+        case 0:
+            tree[index] = tree[2 * index + 1] + tree[2 * index + 2];
+            return;
+        case 1:
+            tree[index] = std::max(tree[2 * index + 1], tree[2 * index + 2]);
+            return;
+        case 2:
+            tree[index] = std::min(tree[2 * index + 1], tree[2 * index + 2]);
+            return;
+        case 3:
+            tree[index] = (tree[2 * index + 1] ^ tree[2 * index + 2]);
+            return;
+        }
+    }
+
+    // Utility function for range_update
+    void _range_update(int start, int end, int index = 0)
+    {
+        // Update a point within range
+        if (start == end)
+        {
+            // For vector base / array base
+            switch (base_type)
+            {
+            case 0:
+                tree[index] = base[start - u_start]; // to get real index
+                return;
+            case 1:
+                tree[index] = (*vec)[start - u_start]; // to get real index
+                return;
+            }
+        }
+        // Partial overlap
+        int mid = start + ((end - start) / 2);
+
+        // Search in the left sub-tree
+        if (mid >= u_start)
+            _range_update(start, mid, 2 * index + 1);
+
+        // Search in the left right-tree
+        if (mid < u_end)
+            _range_update(mid + 1, end, 2 * index + 2);
+
+        // To switch different types of segement trees
+        switch (type)
+        {
+        case 0:
+            tree[index] = tree[2 * index + 1] + tree[2 * index + 2];
+            return;
+        case 1:
+            tree[index] = std::max(tree[2 * index + 1], tree[2 * index + 2]);
+            return;
+        case 2:
+            tree[index] = std::min(tree[2 * index + 1], tree[2 * index + 2]);
+            return;
+        case 3:
+            tree[index] = (tree[2 * index + 1] ^ tree[2 * index + 2]);
+            return;
+        }
+    }
+
 public:
     /*
     type defines type of Segement Tree avaliable options:
@@ -118,19 +219,22 @@ public:
     2 - Min Segement Tree
     3 - XOR Segement Tree
     */
-    SegTree(const int *start, const int *last, int type = 0)
+    SegTree(int *start, int *last, int type = 0)
     {
         // Saving the type of segement tree
         this->type = type;
+        base_type = 0;
 
         n = last - start;
         size = 4 * (n);
+
+        // saving pointer to start of array
+        base = start;
 
         // Trying to allocate memory for segement tree
         try
         {
             tree = new int[++size];
-            base = new int[n];
         }
         catch (const std::exception &e)
         {
@@ -138,17 +242,85 @@ public:
             return;
         }
 
+        int init = 0;
+        switch (type)
+        {
+        case 1:
+            init = INT32_MIN;
+            break;
+        case 2:
+            init = INT32_MAX;
+            break;
+        }
+
         // Initialization of segement tree array
         // and copying data from user provided array
         int i = 0;
         for (; i < n; ++i)
-            base[i] = start[i], tree[i] = 0;
+            base[i] = start[i], tree[i] = init;
 
         for (; i < size; ++i)
-            tree[i] = 0;
+            tree[i] = init;
 
         // After initialization building tree
         buildTree(0, n - 1, 0);
+
+        // clearing pointer to user provided array
+        base = nullptr;
+    }
+
+    /*
+    type defines type of Segement Tree avaliable options:
+    0 - Sum Segement Tree
+    1 - Max Segement Tree
+    2 - Min Segement Tree
+    3 - XOR Segement Tree
+    */
+    SegTree(std::vector<int> &arr, int type = 0)
+    {
+        // Saving the type of segement tree
+        this->type = type;
+        base_type = 1;
+
+        n = arr.size();
+        size = 4 * (n);
+
+        // storing a pointer to vector
+        // avoids making class object bulky
+        vec = &arr;
+
+        // Trying to allocate memory for segement tree
+        try
+        {
+            tree = new int[++size];
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+            return;
+        }
+
+        int init = 0;
+        switch (type)
+        {
+        case 1:
+            init = INT32_MIN;
+            break;
+        case 2:
+            init = INT32_MAX;
+            break;
+        }
+
+        // Initialization of segement tree array
+        // and copying data from user provided array
+        for (int i = 0; i < size; ++i)
+            tree[i] = init;
+
+        // After initialization building tree
+        buildTree(0, n - 1, 0);
+
+        // clearing pointer to user provided vector
+        vec = nullptr;
     }
 
     /*
@@ -162,15 +334,19 @@ public:
     {
         // Saving the type of segement tree
         this->type = type;
+        base_type = 1;
 
         n = arr.size();
         size = 4 * (n);
+
+        // storing a pointer to vector
+        // avoids making class object bulky
+        vec = &arr;
 
         // Trying to allocate memory for segement tree
         try
         {
             tree = new int[++size];
-            base = new int[n];
         }
         catch (const std::exception &e)
         {
@@ -178,26 +354,32 @@ public:
             return;
         }
 
+        int init = 0;
+        switch (type)
+        {
+        case 1:
+            init = INT32_MIN;
+            break;
+        case 2:
+            init = INT32_MAX;
+            break;
+        }
+
         // Initialization of segement tree array
         // and copying data from user provided array
-        int i = 0;
-        for (; i < n; ++i)
-            base[i] = arr[i], tree[i] = 0;
-
-        for (; i < size; ++i)
-            tree[i] = 0;
+        for (int i = 0; i < size; ++i)
+            tree[i] = init;
 
         // After initialization building tree
         buildTree(0, n - 1, 0);
+
+        // clearing pointer to user provided vector
+        vec = nullptr;
     }
 
-    // Answers a query of valid range, returns -1 otherwise
+    // Answers a query of valid range (O (log n)), returns -1 otherwise O(1)
     int query(int start, int end)
     {
-        // No need to call function for just 1 index
-        if (start == end)
-            return base[start];
-
         // Invalid range
         if (start > end or start < 0 or end >= n)
             return -1;
@@ -212,11 +394,78 @@ public:
         return _query(0, n - 1, 0);
     }
 
+    // Update a single index
     void point_update(int index, int update)
     {
+        if (index < 0 or index >= n)
+            return;
+
+        // this is to reduce number of paramaters in
+        // utility function call
+        q_start = index;
+        q_end = update;
+
+        _update(0, n - 1, 0);
     }
 
-    void range_update(int start, int end, const int *first, const int *last)
+    /* 
+    range_update has three types of overloads 
+    */
+    
+    /*
+    start, end => (inclusive)indices of the base/provided array
+    update => vector of update values 
+    */
+    void range_update(int start, int end, std::vector<int> &update)
     {
+        if (end - start + 1 != update.size())
+            return;
+
+        base_type = 1;
+        u_start = start, u_end = end;
+        vec = &update;
+
+        q_start = start, q_end = end;
+        _range_update(0, n - 1, 0);
+
+        vec = nullptr;
+    }
+
+    /*
+    start, end => (inclusive)indices of the base/provided array
+    update => vector of update values 
+    */
+    void range_update(int start, int end, std::vector<int> arr)
+    {
+        if (end - start + 1 != arr.size())
+            return;
+
+        base_type = 1;
+        u_start = start, u_end = end;
+        vec = &arr;
+
+        q_start = start, q_end = end;
+        _range_update(0, n - 1, 0);
+
+        vec = nullptr;
+    }
+
+    /*
+    start, end => (inclusive)indices of the base/provided array
+    array range - [first, last)
+    */
+    void range_update(int start, int end, int *first, int *last)
+    {
+        if (end - start + 1 != last - first)
+            return;
+
+        base_type = 0;
+        u_start = start, u_end = end;
+        base = first;
+
+        q_start = start, q_end = end;
+        _range_update(0, n - 1, 0);
+
+        base = nullptr;
     }
 };
--- Segment Trees/test.cpp
+++ Segment Trees/test.cpp
@@ -4,14 +4,29 @@ using namespace std;
 
 int main()
 {
-  /*  int arr[] = {1, 2, 5, 9, -1, 2, 4, -7};
+	/*  int arr[] = {1, 2, 5, 9, -1, 2, 4, -7};
     int size = sizeof(arr) / sizeof(arr[0]);
 
     SegTree mine(arr, arr + size, 0);
 
     cout << mine.query(2, 2);
 */
-    SegTree vec(vector<int>{1, 2, 5, 9, -1, 2, 4, -7});
+	SegTree vec(vector<int>{1, 2, 5, 9, -1, 2, 4, -7}, 1);
 
-    cout << vec.query(0, 7);
+	cout << vec.query(0, 7) << '\n';
+/*
+	for (int i = 0; i < vec.size; ++i)
+		cout << vec.tree[i] << ' ';
+*/
+	cout << "\n\n";/*
+	int arr[] = {5, 7, 3, 1};
+	vec.range_update(4, 7, arr, arr + 4);
+	*/
+	vec.point_update(5, 100);
+	/*for (int i = 0; i < vec.size; ++i)
+		cout << vec.tree[i] << ' ';
+
+	cout << "\n\n";
+	cout << '\n';*/
+	cout << vec.query(0, 7);
 }
